b7f8e4ea7de6c3660d17930fcc403311b223e6ac
zhangbaojun <jay@4zlink.com>
2017-05-16 15:46:44 +0800

    111

--- servicezuul/src/test/java/com/example/demo/ServiceZuulApplicationTests.java
+++ servicezuul/src/test/java/com/example/demo/ServiceZuulApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServicezuulApplicationTests {
+public class ServiceZuulApplicationTests {
 
 	@Test
 	public void contextLoads() {
