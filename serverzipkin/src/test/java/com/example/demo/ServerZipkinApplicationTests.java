d4c0ea5dd755a5f06ba2ffb70722f1e86c7a164a
zhangbaojun <jay@4zlink.com>
2017-06-12 19:30:03 +0800

    111

--- serverzipkin/src/test/java/com/example/demo/ServerZipkinApplicationTests.java
+++ serverzipkin/src/test/java/com/example/demo/ServerZipkinApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServerzipkinApplicationTests {
+public class ServerZipkinApplicationTests {
+
 
 	@Test
 	public void contextLoads() {
